4ee0ee8af64971e3db3bacde0fc677bd27bfd613
Ali Sattari <ali.sattari@gmail.com>
2025-02-28 19:31:08 +0100

    day 1 part 2

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,4 @@
-def solution(input: str) -> int:
+def solution_p1(input: str) -> int:
     nums = []
     with open(input) as file:
         for line in file:
@@ -15,7 +15,45 @@ def solution(input: str) -> int:
 
     return answer
 
+def solution_p2(input: str) -> int:
+    digit_words = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    nums = []
+    with open(input) as file:
+        for line in file:
+            l = []
+            for i, char in enumerate(line):
+                if char.isdigit():
+                    l.append(char)
+                else:
+                    for w, d in digit_words.items():
+                        if line.startswith(w, i):
+                            l.append(d)
+            # print(line, l)
+            nums.append(l)
+    
+    answer = 0
+    for row in nums:
+        n = int(f"{row[0]}{row[-1]}")
+        answer += n
+
+    return answer
+
 if __name__ == "__main__":
     p1_sample_answer = 142
-    print("Part 1 sample:", solution('./p1_sample.input') == p1_sample_answer)
-    print("Part 1 main:", solution('./p1_main.input'))
+    print("Part 1 sample:", solution_p1('./p1_sample.input') == p1_sample_answer)
+    print("Part 1 main:", solution_p1('./p1_main.input'))
+
+    p2_sample_answer = 281
+    p2_sample_result = solution_p2('./p2_sample.input')
+    print("Part 2 sample:", p2_sample_result == p2_sample_answer, p2_sample_result)
+    print("Part 2 main:", solution_p2('./p1_main.input'))
