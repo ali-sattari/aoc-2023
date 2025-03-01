6da21196b84f5c6ab166e0ad557ba94f9a06f457
Ali Sattari <ali.sattari@gmail.com>
2025-03-01 09:32:53 +0100

    some code cleanup and comments

--- 1/main.py
+++ 1/main.py
@@ -6,14 +6,9 @@ def solution_p1(input: str) -> int:
             for char in line:
                 if char.isdigit():
                     l.append(char)
-            nums.append(l)
+            nums.append(int(f"{l[0]}{l[-1]}"))
     
-    answer = 0
-    for row in nums:
-        n = int(f"{row[0]}{row[-1]}")
-        answer += n
-
-    return answer
+    return sum(nums)
 
 def solution_p2(input: str) -> int:
     digit_words = {
@@ -31,26 +26,26 @@ def solution_p2(input: str) -> int:
     with open(input) as file:
         for line in file:
             l = []
-            for i, char in enumerate(line):
+            for i in range(len(line)):
+                char = line[i]
                 if char.isdigit():
                     l.append(char)
                 else:
                     for w, d in digit_words.items():
                         if line.startswith(w, i):
                             l.append(d)
-            # print(line, l)
-            nums.append(l)
+                            # jump ahead by the length of the matched word
+                            i += len(w)
+                            # why continue once we match any of the words?
+                            break
+            nums.append(int(f"{l[0]}{l[-1]}"))
     
-    answer = 0
-    for row in nums:
-        n = int(f"{row[0]}{row[-1]}")
-        answer += n
-
-    return answer
+    return sum(nums)
 
 if __name__ == "__main__":
     p1_sample_answer = 142
-    print("Part 1 sample:", solution_p1('./p1_sample.input') == p1_sample_answer)
+    p1_sample_result = solution_p1('./p1_sample.input')
+    print("Part 1 sample:", p1_sample_result == p1_sample_answer, p1_sample_result)
     print("Part 1 main:", solution_p1('./p1_main.input'))
 
     p2_sample_answer = 281
