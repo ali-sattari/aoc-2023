05c49e6f1b404d7dd03daae32f05c3295f24e0b4
Ali Sattari <ali.sattari@gmail.com>
2025-03-07 21:56:58 +0100

    day 4 part 2

--- 4/main.py
+++ 4/main.py
@@ -1,5 +1,4 @@
-
-
+from collections import defaultdict
 from pprint import pprint
 
 def calculate_points(cards: list[tuple[set,set]]) -> int:
@@ -10,6 +9,51 @@ def calculate_points(cards: list[tuple[set,set]]) -> int:
             total += 2**(count-1)
 
     return total
+
+def count_cards(cards: list[tuple[set,set]]) -> int:
+    # mem = dict()
+    # res = len(cards)
+    # for i, card in enumerate(cards):
+    #     res += count_cards_recursive(cards, i, mem)
+
+    res = count_cards_iterative(cards)
+
+    return res
+
+def count_cards_iterative(cards: list[tuple[set,set]]) -> int:
+    matches = []
+    for card in cards:
+        w, h = card
+        matches.append(len(w & h))
+
+    copies = [1]*len(cards)
+    for i in range(len(cards)):
+        next_cards = min(i+1+matches[i], len(cards))
+        for j in range(i+1, next_cards):
+            copies[j] += copies[i]
+
+    return sum(copies)
+
+def count_cards_recursive(cards: list[tuple[set,set]], index: int, mem:dict[int,int]) -> int:
+    if index >= len(cards):
+        return 0
+
+    if index in mem:
+        count = mem[index]
+    else:
+        winner, holding = cards[index]
+        count = len(winner & holding)
+        mem[index] = count
+
+    if index == len(cards)-1:
+        return count
+
+    res = count
+    for i in range(1, count+1):
+        c = count_cards_recursive(cards, index+i, mem)
+        res += c
+
+    return res
 
 def parse_input(path: str) -> list:
     out = []
@@ -28,7 +72,12 @@ if __name__ == "__main__":
     sample_answer_p1 = 13
     sample_result_p1 = calculate_points(sample_input)
     print("Part 1 sample:", sample_answer_p1 == sample_result_p1, sample_result_p1)
+    sample_answer_p2 = 30
+    sample_result_p2 = count_cards(sample_input)
+    print("Part 2 sample:", sample_answer_p2 == sample_result_p2, sample_result_p2)
 
     main_input = parse_input("./main.input")
     main_result_p1 = calculate_points(main_input)
     print("Part 1 main:", main_result_p1)
+    main_result_p2 = count_cards(main_input)
+    print("Part 2 main:", main_result_p2)
--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Doing [Advent of Code 2023](https://adventofcode.com/2023/) for fun and practice
 
 For future reference, what I used to solved each day.
 
-| Day | Data Structures | Algorithms | Remarks |
-|-----|-----------------|------------|---------|
-| 1   | hashmap         | - | - |
-| 2   | tuple           | - | - |
-| 3   | matrix, tuple   | grid (adjacency, boundary) | - |
-| 4   | set             | - | - |
+| Day | Topics | Remarks |
+|-----|--------|---------|
+| 1   | hashmap         | - |
+| 2   | tuple           | - |
+| 3   | matrix, tuple, grid (adjacency, boundary) | - |
+| 4   | set, recursion, dynamic programming | - |
